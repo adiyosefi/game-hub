df1a7408bcb6c9148ab20f522cbb210806683860
adi.nomberg <adi.nomberg@1touch.io>
2024-02-22 12:20:03 +0200

    move api key to env variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '8a21cc5a2ea34354968504f8dfa10541'
+        key: import.meta.env.VITE_RAWG_API_KEY
     }
 })
