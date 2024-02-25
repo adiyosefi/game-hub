3fc6606a8b2b3f6bd6f2386cd69f1faa3cbb58a2
adi.nomberg <adi.nomberg@1touch.io>
2024-02-25 12:41:20 +0200

    build platform selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = ({selectedGenre}: Props) => {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding={10} spacing={3}>
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding="10px" spacing={6}>
                 {isLoading && skeletons.map(skeleton => (
                     <GameCardContainer key={skeleton}>
                         <GameCardSkeleton/>
